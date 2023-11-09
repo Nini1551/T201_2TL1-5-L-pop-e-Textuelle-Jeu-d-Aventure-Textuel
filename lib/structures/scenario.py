567db77604e39c8c585e314b8674f018264d3131
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2023-11-09 15:50:18 +0100

    MVP - Implémentation de la version 0.1.2 Amélioration de la lisibilité du code

--- lib/structures/scenario.py
+++ lib/structures/scenario.py
@@ -6,20 +6,26 @@ class Aventure:
     Représente une aventure jouée par un joueur.
     L'aventure suit les choix de celui-ci et se termine quand l'aventure est terminée (quand il n'y a plus de choix disponibles)
     """
+    LIB_TAG = 'lib'
+    TEXT_TAG = 'text'
+    CHOICES_TAG = 'choices'
+
     def __init__(self, scenario_path: str):
         """
         Initialise une aventure via un premier choix généralement appelé introduction
         PRE: - scenario_path: le nom du fichier JSON du scénario joué par le joueur.
-               le fichier JSON doit avoir un format précis. Il s'agit d'un dictionnaire représentant un choix.
+               Le fichier JSON doit avoir un format précis. Il s'agit d'un dictionnaire représentant un choix.
                 Il est composé de plusieurs attributs :
-                - lib: une chaîne de caractères donnant l'intitulé du choix en une ligne.
-                - text: le texte de narration lié au choix. Il doit terminer par un retour à la ligne
-                - choices: une liste qui sera soit vide, soit constituée elle-même de choix (suivre ce format-ci)
-        POST: Initialise une aventure constitué de l'attribut suivant :
+                - lib : une chaîne de caractères donnant l'intitulé du choix en une ligne.
+                - text : le texte de narration lié au choix. Il doit terminer par un retour à la ligne
+                - choices : une liste qui sera soit vide, soit constituée elle-même de choix (suivre ce format-ci)
+        POST: Initialise une aventure constituée de l'attribut suivant :
               - main_choice : le choix actuel de l'aventure.
                               Il est initialisé sur le choix d'introduction contenu dans le dictionnaire du document JSON ouvert via 'scenario_path'.
         """
-        with open(scenario_path, encoding='utf-8') as scenario_file:
+        encoding = 'utf-8'
+
+        with open(scenario_path, encoding=encoding) as scenario_file:
             self.__main_choice = json.load(scenario_file)
 
     def __str__(self) -> str:
@@ -28,9 +34,9 @@ class Aventure:
               - le texte de narration du choix actuel suivi d'un passage à la ligne.
               - une liste numérotée des libellés des sous-choix du choix actuel.
         """
-        to_display = f"{self.__main_choice['text']}\n"
-        for i, choice in enumerate(self.__main_choice['choices']):
-            to_display += f"{i+1}. {choice['lib']}\n"
+        to_display = f"{self.__main_choice[self.TEXT_TAG]}\n"
+        for i, choice in enumerate(self.__main_choice[self.CHOICES_TAG]):
+            to_display += f"{i+1}. {choice[self.LIB_TAG]}\n"
         return to_display[:-1]
 
     def set_new_choice(self):
@@ -40,19 +46,23 @@ class Aventure:
         Le programme repose la question si le joueur ne rentre pas un entier valide et l'avertit s'il ne rentre pas un nombre.
         POST: Change le choix actuel pour le sous-choix sélectionné par le joueur.
         """
+        error_value = -1
+        error_message = "Insérez un nombre entier valide !"
+        prompt = '>>> '
+
         print(self)
 
-        choices = self.__main_choice['choices']
-        answer = -1
-        while answer == -1:
-            answer = input('>>> ')
+        choices = self.__main_choice[self.CHOICES_TAG]
+        answer = error_value
+        while answer == error_value:
+            answer = input(prompt)
             try:
                 answer = int(answer) - 1
                 if answer not in range(len(choices)):
                     raise ValueError
             except ValueError:
-                print("\nInsérez un nombre entier valide !")
-                answer = -1
+                print(f"\n{error_message}")
+                answer = error_value
 
         self.__main_choice = choices[answer]
 
@@ -60,7 +70,7 @@ class Aventure:
         """
         POST: Renvoie si le choix actuel est un choix final, c'est à dire s'il a lui-même un ou plusieurs sous-choix.
         """
-        return not len(self.__main_choice['choices'])
+        return not len(self.__main_choice[self.CHOICES_TAG])
 
     def run(self):
         """
@@ -68,7 +78,9 @@ class Aventure:
         Quand ce n'est plus le cas, affiche un message de félicitations.
         POST: Parcoure le scénario de l'aventure jusqu'à ce que le joueur ne puisse plus faire de choix.
         """
+        congratulations_message = "Félicitations ! Vous avez terminé l'aventure. \nN'hésitez pas à redémarrer le programme pour tester les autres choix disponibles !"
+
         while not self.is_final_choice():
             self.set_new_choice()
         print(self)
-        print("Félicitations ! Vous avez terminé l'aventure. \nN'hésitez pas à redémarrer le programme pour tester les autres choix disponibles !")
+        print(congratulations_message)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 En tant que joueur débutant ou chevronné, Partez à l'aventure.
 Le joueur va parcourir l'aventure tutorielle jusqu'à ce qu'il finisse.
 Lorsqu'il termine, le joueur est félicité et est invité à recommencer l'aventure pour essayer de nouveau choix.
+
+Auteurs : 2TL1-5
 """
 from lib.structures.scenario import Aventure
 
